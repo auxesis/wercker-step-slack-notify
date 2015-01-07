1cf3c2faf31db84dd4abc6e9a7ad534037a5c36b
Lindsay Holmwood <lindsay@holmwood.id.au>
2015-01-07 19:45:50 +1100

    Update variable names to match werker step package name

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Argument checking
-if [ -z "$WERCKER_SLACK_NOTIFY_WEBHOOK_URL" ]; then
+if [ -z "$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_URL" ]; then
   fail "Please specify WEBHOOK_URL"
   exit 1
 fi
@@ -10,24 +10,24 @@ fi
 payload="payload={"
 
 # Channel
-if [ -n "$WERCKER_SLACK_NOTIFY_CHANNEL" ]; then
-  payload="$payload \"channel\":\"$WERCKER_SLACK_NOTIFY_CHANNEL\","
+if [ -n "$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_CHANNEL" ]; then
+  payload="$payload \"channel\":\"$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_CHANNEL\","
 else
   payload="$payload \"channel\": \"#general\","
 fi
 
 # Username
-if [ -n "$WERCKER_SLACK_NOTIFY_USERNAME" ]; then
-  payload="$payload \"username\":\"$WERCKER_SLACK_NOTIFY_USERNAME\","
+if [ -n "$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_USERNAME" ]; then
+  payload="$payload \"username\":\"$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_USERNAME\","
 else
   payload="$payload \"username\": \"Wercker\","
 fi
 
 # Icon
-if [ -n "$WERCKER_SLACK_NOTIFY_ICON_URL" ]; then
-  payload="$payload \"icon_url\":\"$WERCKER_SLACK_NOTIFY_ICON_URL\","
-elif [ -n "$WERCKER_SLACK_NOTIFY_ICON_EMOJI" ]; then
-  payload="$payload \"icon_emoji\":\"$WERCKER_SLACK_NOTIFY_ICON_EMOJI\","
+if [ -n "$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_ICON_URL" ]; then
+  payload="$payload \"icon_url\":\"$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_ICON_URL\","
+elif [ -n "$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_ICON_EMOJI" ]; then
+  payload="$payload \"icon_emoji\":\"$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_ICON_EMOJI\","
 else
   payload="$payload \"icon_url\": \"https://avatars3.githubusercontent.com/u/1695193?s=140\","
 fi
@@ -35,17 +35,17 @@ fi
 # Message
 
 # Build up all the possible messages we can send to Slack
-if [ -n "$WERCKER_SLACK_NOTIFY_PASSED_MESSAGE" ]; then
-  export BUILD_PASSED_MESSAGE=$WERCKER_SLACK_NOTIFY_PASSED_MESSAGE
-  export DEPLOY_PASSED_MESSAGE=$WERCKER_SLACK_NOTIFY_PASSED_MESSAGE
+if [ -n "$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_PASSED_MESSAGE" ]; then
+  export BUILD_PASSED_MESSAGE=$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_PASSED_MESSAGE
+  export DEPLOY_PASSED_MESSAGE=$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_PASSED_MESSAGE
 else
   export BUILD_PASSED_MESSAGE=":white_check_mark: $WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: <$WERCKER_BUILD_URL|build> of $WERCKER_GIT_BRANCH by $WERCKER_STARTED_BY passed."
   export DEPLOY_PASSED_MESSAGE=":white_check_mark: $WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: <$WERCKER_DEPLOY_URL|deploy of $WERCKER_GIT_BRANCH> to $WERCKER_DEPLOYTARGET_NAME by $WERCKER_STARTED_BY passed."
 fi
 
-if [ -n "$WERCKER_SLACK_NOTIFY_FAILED_MESSAGE" ]; then
-  export BUILD_FAILED_MESSAGE=$WERCKER_SLACK_NOTIFY_FAILED_MESSAGE
-  export DEPLOY_FAILED_MESSAGE=$WERCKER_SLACK_NOTIFY_FAILED_MESSAGE
+if [ -n "$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_FAILED_MESSAGE" ]; then
+  export BUILD_FAILED_MESSAGE=$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_FAILED_MESSAGE
+  export DEPLOY_FAILED_MESSAGE=$WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_FAILED_MESSAGE
 else
   export BUILD_FAILED_MESSAGE=":no_entry: $WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: <$WERCKER_BUILD_URL|build> of $WERCKER_GIT_BRANCH by $WERCKER_STARTED_BY failed."
   export DEPLOY_FAILED_MESSAGE=":no_entry: $WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: <$WERCKER_DEPLOY_URL|deploy> of $WERCKER_GIT_BRANCH to $WERCKER_DEPLOYTARGET_NAME by $WERCKER_STARTED_BY failed."
@@ -69,8 +69,12 @@ payload="$payload }" # Close the JSON document
 debug $payload
 
 RESPONSE_OUTPUT="$WERCKER_STEP_TEMP/body.log"
-RESPONSE_CODE=$(curl -s -X POST --data-urlencode "$payload" $WERCKER_SLACK_NOTIFY_WEBHOOK_URL --output $RESPONSE_OUTPUT -w "%{http_code}")
+RESPONSE_CODE=$(curl -s -X POST --data-urlencode "$payload" $WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_URL --output $RESPONSE_OUTPUT -w "%{http_code}")
 RETVAL=$?
+
+if [ "$RESPONSE_CODE" = "000" ]; then
+  fail "Couldn't connect to $WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_URL"
+fi
 
 if [ "$RESPONSE_CODE" = "500" ]; then
   fail "$(cat $RESPONSE_OUTPUT)"
