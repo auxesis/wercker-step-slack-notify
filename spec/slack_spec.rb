5050e464cfa2708a1f22332423816dd46f95af51
Lindsay Holmwood <lindsay@holmwood.id.au>
2015-01-08 09:03:04 +1100

    Add explanatory comments. Fix indentation. Set default env vars for deploy

--- spec/slack_spec.rb
+++ spec/slack_spec.rb
@@ -1,19 +1,24 @@
 describe 'slack-notify-via-webhook' do
+  # Common environment variables used all notification types.
   let(:defaults) { {
     "WERCKER_APPLICATION_OWNER_NAME" => "adalovelace",
     "WERCKER_APPLICATION_NAME"       => "analytical_engine",
     "WERCKER_GIT_BRANCH"             => "master",
     "WERCKER_STARTED_BY"             => "Ada Lovelace",
-    "WERCKER_DEPLOYTARGET_NAME"      => "production"
+    "WERCKER_DEPLOYTARGET_NAME"      => "production",
   } }
 
+  # Build environment variables
   let(:build) { {
-    "WERCKER_BUILD_URL"              => "https://app.wercker.com/#build/decafc0ffee",
+    "WERCKER_BUILD_URL" => "https://app.wercker.com/#build/decafc0ffee",
+    "DEPLOY"            => false,
   } }
 
+  # Deploy environment variables
   let(:deploy) { {
-    "WERCKER_DEPLOY_URL"              => "https://app.wercker.com/#deploy/c0ffeefacade",
-    "WERCKER_DEPLOYTARGET_NAME"      => "production"
+    "WERCKER_DEPLOY_URL"        => "https://app.wercker.com/#deploy/c0ffeefacade",
+    "WERCKER_DEPLOYTARGET_NAME" => "production",
+    "DEPLOY"                    => true,
   } }
 
   it 'validates webhook url is set' do
@@ -26,7 +31,6 @@ describe 'slack-notify-via-webhook' do
     environment = defaults.merge(build).merge({
       'WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_WEBHOOK_URL' => 'http://localhost:9900/hello',
       'WERCKER_RESULT' => 'success',
-      'DEPLOY'         => false
     })
 
     runner(environment, fail_on_error=false)
@@ -45,7 +49,7 @@ describe 'slack-notify-via-webhook' do
     expect(last_slack_request['channel']).to eq "#general"
   end
 
-  it 'notifies on passed build' do
+  it 'notifies on passed build with canned message' do
     environment = defaults.merge(build).merge({
       'WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_WEBHOOK_URL' => 'http://localhost:9988/passed_build',
       'WERCKER_RESULT' => 'passed',
@@ -63,7 +67,7 @@ describe 'slack-notify-via-webhook' do
     expect(last_slack_request['text']).to include("<" + environment["WERCKER_BUILD_URL"] + "|build>")
   end
 
-  it 'notifies on failed build' do
+  it 'notifies on failed build with canned message' do
     environment = defaults.merge(build).merge({
       'WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_WEBHOOK_URL' => 'http://localhost:9988/failed_build',
       'WERCKER_RESULT' => 'failed',
@@ -81,11 +85,10 @@ describe 'slack-notify-via-webhook' do
     expect(last_slack_request['text']).to include("<" + environment["WERCKER_BUILD_URL"] + "|build>")
   end
 
-  it 'notifies on passed deploy' do
+  it 'notifies on passed deploy with canned message' do
     environment = defaults.merge(deploy).merge({
       'WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_WEBHOOK_URL' => 'http://localhost:9988/passed_deploy',
       'WERCKER_RESULT' => 'passed',
-      'DEPLOY'         => true
     })
 
     runner(environment)
@@ -100,11 +103,10 @@ describe 'slack-notify-via-webhook' do
     expect(last_slack_request['text']).to include("<" + environment["WERCKER_DEPLOY_URL"] + "|deploy>")
   end
 
-  it 'notifies on failed deploy' do
+  it 'notifies on failed deploy with canned message' do
     environment = defaults.merge(deploy).merge({
       'WERCKER_SLACK_NOTIFY_VIA_WEBHOOK_WEBHOOK_URL' => 'http://localhost:9988/failed_build',
       'WERCKER_RESULT' => 'failed',
-      'DEPLOY'         => true
     })
 
     runner(environment)
@@ -202,6 +204,13 @@ describe 'slack-notify-via-webhook' do
   end
 end
 
+# Wercker steps are executed on the Wercker platform by setting up an
+# environment, then sourcing a user supplied script. When testing, this means
+# you can't just execute `run.sh` - you need a parent script that sets up
+# similar environment variables, then sources `run.sh`.
+#
+# The runner method does exactly that - it dynamically generates a shellscript
+# from supplied environment variables, and executes the generated script.
 def runner(environment, fail_on_error=true)
   # Generate the runner
   template = File.read("spec/support/runner.sh.erb")
@@ -212,9 +221,16 @@ def runner(environment, fail_on_error=true)
   filesystem_permissions(0755, 'runner.sh')
   FileUtils.cp('run.sh', current_dir)
 
+  # Execute the runner
   run_simple "#{current_dir}/runner.sh", fail_on_error
 end
 
+# During rspec initialisation we set up a web server that mocks out responses
+# we would expect from Slack. When the web server receives a requet, it writes
+# it into a global variable, for inspection by the tests.
+#
+# last_slack_request is a helper method to get the last request send to the mock
+# Slack server.
 def last_slack_request
   $SLACK_REQUEST_QUEUE.last
 end
